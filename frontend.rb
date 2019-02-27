ef4cc0e0546a437768ab9a772182d450a351c23d
Lindsey Katz <katz.lindsey@gmail.com>
2019-02-26 20:32:03 -0600

    Create tty table

--- frontend.rb
+++ frontend.rb
@@ -1,13 +1,17 @@
 require "HTTP"
+
+require "tty-table"
 # puts "Welcome to my jacket store!"
 # puts "Press 1 to get details on a random jacket."
 # user_input = gets.chomp
 # if user_unput == 1
-  my_product = HTTP.get("http://localhost:3000/api/product_url")
-  product = my_product.parse
+my_product = HTTP.get("http://localhost:3000/api/product_url")
+product = my_product.parse
   # jacket = product["products="]
   # random_product = []
   # random_product << product["products="].rand(3)
   # p random_product
-  p product
-# end
+p product
+
+table = TTY::Table.new ['header1','header2'], [['a1', 'a2'], ['b1', 'b2']]
+puts table.render(:ascii)
